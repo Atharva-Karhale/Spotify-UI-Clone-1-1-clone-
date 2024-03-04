86dbd13cbdf1eed7709f8d4d4e994673e98bb95b
Atharva-Karhale <pirateask6@gmail.com>
2024-03-04 06:39:50 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import RightBar from '/src/components/RightBar';
+import SearchPage from './components/SearchPage';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<RightBar/>}/>
+      <Route path='/search' element={<SearchPage/>}/>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <meta name="viewport" content="width=device-width, initial-scale=0.8" />
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
